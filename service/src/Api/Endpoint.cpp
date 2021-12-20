17aa6c4f77ad5778052fb9f94fb058529c1ad012
tomLamprecht <79751041+tomLamprecht@users.noreply.github.com>
2021-12-20 21:09:36 +0100

    todo list added

--- service/src/Api/Endpoint.cpp
+++ service/src/Api/Endpoint.cpp
@@ -6,6 +6,20 @@ using namespace Reminder::Api;
 using namespace Reminder::Core;
 using namespace crow;
 
+/*
+  GET Reminder
+  Post Reminder
+  PUT Reminder
+  DELETE Reminder
+
+  Get List
+  POST List
+  PUT List
+  DELETE List
+
+  GET Board
+
+*/
 Endpoint::Endpoint(SimpleApp &givenApp, Manager &givenManager) : app(givenApp),
                                                                  manager(givenManager) {
     registerRoutes();
@@ -15,4 +29,5 @@ Endpoint::~Endpoint() {
 }
 
 void Endpoint::registerRoutes() {
+  
 }
