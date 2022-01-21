7af4690af1ab448e0a8abfccb4bd51e5133a9856
tomLamprecht <79751041+tomLamprecht@users.noreply.github.com>
2022-01-21 22:08:43 +0100

    Refactored Frontend to use Modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ListDisplayerComponent } from 'src/lib/ui/listDisplayer/listDisplayer.component';
-import { ListComponent } from 'src/lib/ui/list/list.component';
-import { BoardComponent } from 'src/lib/ui/board/board.component';
-import { ReminderItemComponent } from 'src/lib/ui/reminderItem/reminderItem.component';
-import { ToolbarComponent } from 'src/lib/ui/toolbar/toolbar.component';
+import { UiModule } from 'src/lib/ui/ui.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListHeaderComponent } from 'src/lib/ui/listHeader/listHeader.component';
-import { ListHeaderContainerComponent } from 'src/lib/ui/listHeaderContainer/listHeaderContainer.component';
-import { HttpClientModule} from '@angular/common/http'
 
 
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent, ListDisplayerComponent, ListComponent, ReminderItemComponent, BoardComponent, ListHeaderComponent, ListHeaderContainerComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ frontend/src/lib/ui/ui.module.ts
@@ -0,0 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BoardComponent } from 'src/lib/ui/board/board.component';
+import { ListComponent } from 'src/lib/ui/list/list.component';
+import { ListDisplayerComponent } from 'src/lib/ui/listDisplayer/listDisplayer.component';
+import { ListHeaderComponent } from 'src/lib/ui/listHeader/listHeader.component';
+import { ListHeaderContainerComponent } from 'src/lib/ui/listHeaderContainer/listHeaderContainer.component';
+import { ReminderItemComponent } from 'src/lib/ui/reminderItem/reminderItem.component';
+import { ToolbarComponent } from 'src/lib/ui/toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [ToolbarComponent, ListDisplayerComponent, ListComponent, ReminderItemComponent, BoardComponent, ListHeaderComponent, ListHeaderContainerComponent],
+  imports: [HttpClientModule, BrowserModule],
+  providers: [],
+  exports: [ToolbarComponent, BoardComponent]
+})
+export class UiModule {}
